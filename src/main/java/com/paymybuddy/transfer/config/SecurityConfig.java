381e8b0a79ae577f6556289c175a849ba016bac3
Kafeinedev <kafeinedev@protonmail.com>
2021-12-14 17:04:48 +0100

    Adding basicsecurity configuration linking custom logging with app

--- src/main/java/com/paymybuddy/transfer/config/SecurityConfig.java
+++ src/main/java/com/paymybuddy/transfer/config/SecurityConfig.java
@@ -1,13 +1,37 @@
 package com.paymybuddy.transfer.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/**").anonymous();
+		http.authorizeRequests().antMatchers("/css/**").permitAll().antMatchers("/**").hasRole("USER").and().formLogin()
+				.loginPage("/login").defaultSuccessUrl("/mytransactions").permitAll().and().rememberMe()
+				.userDetailsService(this.userDetailsService()).and().logout().logoutUrl("/logout");
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
 	}
 }
