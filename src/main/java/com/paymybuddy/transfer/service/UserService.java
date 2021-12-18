30170ad85e75b65c9d1a089b78dd51c98fba1c95
Kafeinedev <kafeinedev@protonmail.com>
2021-12-18 14:22:43 +0100

    Adding creation and update method to userService

--- src/main/java/com/paymybuddy/transfer/service/UserService.java
+++ src/main/java/com/paymybuddy/transfer/service/UserService.java
@@ -1,6 +1,7 @@
 package com.paymybuddy.transfer.service;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,8 +11,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.paymybuddy.transfer.constant.RegexStringPattern;
+import com.paymybuddy.transfer.exception.EntityMissingException;
+import com.paymybuddy.transfer.exception.InvalidArgumentException;
 import com.paymybuddy.transfer.model.User;
 import com.paymybuddy.transfer.repository.UserRepository;
 
@@ -20,6 +25,9 @@ public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder encoder;
 
 	private Logger log = LogManager.getLogger("User Service");
 
@@ -32,5 +40,67 @@ public class UserService implements UserDetailsService {
 
 		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
 				List.of(new SimpleGrantedAuthority("ROLE_USER")));
+	}
+
+	public User createUser(String name, String email, String password) throws InvalidArgumentException {
+		validateName(name);
+		validateEmail(email);
+		validatePassword(password);
+		if (userRepository.findByEmail(email).isPresent()) {
+			log.error("could not create user email already present");
+			throw new InvalidArgumentException();
+		}
+
+		User user = User.builder().email(email).name(name).password(encoder.encode(password)).build();
+		return userRepository.save(user);
+	}
+
+	public User updateUser(String userEmail, String name, String email, String password)
+			throws EntityMissingException, InvalidArgumentException {
+		User user = userRepository.findByEmail(userEmail).orElseThrow(() -> {
+			log.error("Could not update user email not found");
+			return new EntityMissingException();
+		});
+		if (name != null) {
+			validateName(name);
+			user.setName(name);
+		}
+		if (email != null) {
+			validateEmail(email);
+			if (userRepository.findByEmail(email).isPresent()) {
+				log.error("Could not update user email : " + email + "already exist");
+				throw new InvalidArgumentException();
+			}
+			user.setEmail(email);
+		}
+		if (password != null) {
+			validatePassword(password);
+			user.setPassword(encoder.encode(password));
+		}
+		return userRepository.save(user);
+	}
+
+	private boolean validateEmail(String email) throws InvalidArgumentException {
+		if (!Pattern.matches(RegexStringPattern.EMAIL, email)) {
+			log.error("Could not create or update user email invalid");
+			throw new InvalidArgumentException();
+		}
+		return true;
+	}
+
+	private boolean validatePassword(String password) throws InvalidArgumentException {
+		if (!Pattern.matches(RegexStringPattern.PASSWORD, password)) {
+			log.error("Could not create or update user password invalid");
+			throw new InvalidArgumentException();
+		}
+		return true;
+	}
+
+	private boolean validateName(String name) throws InvalidArgumentException {
+		if (name.length() > 16) {
+			log.error("Could not create or update user name too long");
+			throw new InvalidArgumentException();
+		}
+		return true;
 	}
 }
--- src/test/java/com/paymybuddy/transfer/UserServiceTest.java
+++ src/test/java/com/paymybuddy/transfer/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.paymybuddy.transfer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -16,7 +17,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.paymybuddy.transfer.exception.EntityMissingException;
+import com.paymybuddy.transfer.exception.InvalidArgumentException;
 import com.paymybuddy.transfer.model.User;
 import com.paymybuddy.transfer.repository.TransactionRepository;
 import com.paymybuddy.transfer.repository.UserRepository;
@@ -31,6 +35,9 @@ class UserServiceTest {
 
 	@Mock
 	private TransactionRepository mockTransactionRepository;
+
+	@Mock
+	private PasswordEncoder mockPasswordEncoder;
 
 	@Mock
 	private UserRepository mockUserRepository;
@@ -71,4 +78,137 @@ class UserServiceTest {
 
 		verify(mockUserRepository, times(1)).findByEmail("this is an email");
 	}
+
+	@Test
+	void createUser_whenCalled_returnCreatedUser() throws Exception {
+		User user = User.builder().email("email@a.com").id(1L).password("totally encrypted").name("name").build();
+		when(mockUserRepository.save(any(User.class))).thenReturn(user);
+
+		User test = userService.createUser("name", "email@a.com", "P@ssword1");
+
+		assertThat(test.getId()).isEqualTo(1L);
+		assertThat(test.getName()).isEqualTo("name");
+		assertThat(test.getEmail()).isEqualTo("email@a.com");
+		assertThat(test.getPassword()).isEqualTo("totally encrypted");
+	}
+
+	@Test
+	void createUser_whenCalled_UpdateDatabase() throws Exception {
+		when(mockPasswordEncoder.encode("P@ssword1")).thenReturn("totally encrypted");
+
+		userService.createUser("name", "email@a.com", "P@ssword1");
+
+		verify(mockUserRepository, times(1))
+				.save(User.builder().id(0).email("email@a.com").password("totally encrypted").name("name").build());
+	}
+
+	@Test
+	void createUser_whenCalled_encryptPassword() throws Exception {
+		userService.createUser("name", "email@a.com", "P@ssword1");
+
+		verify(mockPasswordEncoder, times(1)).encode("P@ssword1");
+	}
+
+	@Test
+	void createUser_whenUsernameIsTooLong_throwInvalidArgumentException() {
+		assertThrows(InvalidArgumentException.class,
+				() -> userService.createUser("name iiiiiiiiiiiiiiiiiiiiiiiiiiiissssssss LOOOOOOOOOOOOOOOOONG !",
+						"a@a.com", "P@ssword1"));
+	}
+
+	@Test
+	void createUser_whenEmailIsInvalid_throwInvalidArgumentException() {
+		assertThrows(InvalidArgumentException.class, () -> userService.createUser("name", "invalid", "P@ssword1"));
+	}
+
+	@Test
+	void createUser_whenPasswordIsInvalid_throwInvalidArgumentException() {
+		assertThrows(InvalidArgumentException.class, () -> userService.createUser("name", "mail@a.com", "123456"));
+	}
+
+	@Test
+	void createUser_ifEmailAlreadyExist_throwInvalidArgumentException() {
+		when(mockUserRepository.findByEmail("mail@a.com")).thenReturn(Optional.of(new User()));
+
+		assertThrows(InvalidArgumentException.class, () -> userService.createUser("name", "mail@a.com", "P@ssword1"));
+	}
+
+	@Test
+	void updateUser_whenCalled_returnUpdatedUser() throws Exception {
+		User user = new User();
+
+		when(mockUserRepository.findByEmail(any(String.class))).thenReturn(Optional.of(user))
+				.thenReturn(Optional.empty());
+		when(mockUserRepository.save(user)).thenReturn(user);
+		when(mockPasswordEncoder.encode("newP@ssword1")).thenReturn("encrypted");
+
+		User test = userService.updateUser("email@a.com", "new name", "newmail@mail.com", "newP@ssword1");
+
+		assertThat(test.getName()).isEqualTo("new name");
+		assertThat(test.getEmail()).isEqualTo("newmail@mail.com");
+		assertThat(test.getPassword()).isEqualTo("encrypted");
+	}
+
+	@Test
+	void updateUser_whenCalledWithaNewPassword_encryptPassword() throws Exception {
+		User user = new User();
+
+		when(mockUserRepository.findByEmail(any(String.class))).thenReturn(Optional.of(user));
+
+		User test = userService.updateUser("email@a.com", null, null, "newP@ssword1");
+
+		verify(mockPasswordEncoder, times(1)).encode("newP@ssword1");
+	}
+
+	@Test
+	void updateUser_whenCalled_checkAndUpdateDatabase() throws Exception {
+		User user = new User();
+
+		when(mockUserRepository.findByEmail("mel")).thenReturn(Optional.of(user));
+
+		userService.updateUser("mel", "newname", "newmel@a.com", "P@ssWord1");
+
+		verify(mockUserRepository, times(2)).findByEmail(any(String.class));
+		verify(mockUserRepository, times(1)).save(user);
+	}
+
+	@Test
+	void updateUser_ifMissingUser_throwEntityMissingException() {
+		when(mockUserRepository.findByEmail("mail")).thenReturn(Optional.empty());
+
+		assertThrows(EntityMissingException.class, () -> userService.updateUser("mail", null, null, null));
+	}
+
+	@Test
+	void updateUser_whenUsernameIsTooLong_throwInvalidArgumentException() {
+		when(mockUserRepository.findByEmail("toUpdate")).thenReturn(Optional.of(new User()));
+
+		assertThrows(InvalidArgumentException.class, () -> userService.updateUser("toUpdate",
+				"name iiiiiiiiiiiiiiiiiiiiiiiiiiiissssssss LOOOOOOOOOOOOOOOOONG !", "a@a.com", "P@ssword1"));
+	}
+
+	@Test
+	void updateUser_whenEmailIsInvalid_throwInvalidArgumentException() {
+		when(mockUserRepository.findByEmail("toUpdate")).thenReturn(Optional.of(new User()));
+
+		assertThrows(InvalidArgumentException.class,
+				() -> userService.updateUser("toUpdate", "name", "invalid.com", "P@ssword1"));
+	}
+
+	@Test
+	void updateUser_whenPasswordIsInvalid_throwInvalidArgumentException() {
+		when(mockUserRepository.findByEmail("toUpdate")).thenReturn(Optional.of(new User()));
+
+		assertThrows(InvalidArgumentException.class,
+				() -> userService.updateUser("toUpdate", "name", "a@a.com", "123456"));
+	}
+
+	@Test
+	void updateUser_ifEmailAlreadyExist_throwInvalidArgumentException() {
+		when(mockUserRepository.findByEmail(any(String.class))).thenReturn(Optional.of(new User()));
+
+		assertThrows(InvalidArgumentException.class,
+				() -> userService.updateUser("toUpdate", "name", "a@a.com", "P@ssword1"));
+	}
+
 }
